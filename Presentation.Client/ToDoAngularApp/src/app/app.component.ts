e79517e058d72be59ac94b87022b6b2636926a3c
Eslam Kholeef <Eslamk@GSTechnologyCo.com>
2024-12-20 20:55:39 +0200

    To Do Component

--- Presentation.Client/ToDoAngularApp/src/app/app.component.ts
+++ Presentation.Client/ToDoAngularApp/src/app/app.component.ts
@@ -1,13 +1,45 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
+import { Component, OnInit } from '@angular/core';
+import { TodoItem, TodoService } from './services/todo.service';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ToDoAngularApp';
+
+  todos: TodoItem[] = [];
+  newTodoTitle: string = '';
+
+  constructor(private todoService: TodoService) {}
+
+  ngOnInit(): void {
+    this.loadTodos();
+  }
+
+  loadTodos(): void {
+    this.todoService.getTodos().subscribe(todos => this.todos = todos);
+  }
+
+  addTodo(): void {
+    if (!this.newTodoTitle.trim()) {
+      return;
+    }
+
+    const newTodo: Partial<TodoItem> = { title: this.newTodoTitle };
+    this.todoService.createTodo(newTodo).subscribe(todo => {
+      this.todos.push(todo);
+      this.newTodoTitle = '';
+    });
+  }
+
+  completeTodo(todo: TodoItem): void {
+    this.todoService.markAsCompleted(todo.id).subscribe(() => {
+      todo.isCompleted = true;
+    });
+  }
 }
